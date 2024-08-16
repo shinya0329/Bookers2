8e727e818adb4229fd9a4c2dc0bccd69b231445d
EC2 Default User <ec2-user@ip-172-31-26-162.ap-northeast-1.compute.internal>
2024-08-16 06:51:53 +0000

    [Add] bookのdestroy機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as:"about"
-  resources :books, only: [:index, :show, :create, :destroy]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy]
 end
