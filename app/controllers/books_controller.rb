2236572866c09a4242e0238f5287ac3b8b81dab7
EC2 Default User <ec2-user@ip-172-31-26-162.ap-northeast-1.compute.internal>
2024-08-16 05:40:18 +0000

    [Add] booksの一覧画面、詳細画面に必要なcontroller.viewの記述

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,27 @@
 class BooksController < ApplicationController
-  def index
-  end
+  
 
+  def index
+    @book = Book.new
+    @books
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+  
   def show
+    @book = Book.new
+    @book_detail = Book.find(params[:id])
+  end
+  
+  # 投稿データのストロングパラメータ
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get "homes/about" => "homes#about", as:"about"
-  resources :books, only: [:index, :show]
+  resources :books, only: [:index, :show, :crea]
 end
